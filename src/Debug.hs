bd4b2539e926089dc1e8f857a1462e566b42cf51
marklar <markwvh@gmail.com>
2017-01-23 12:30:28 -0800

    mv logfile to /tmp

--- src/Debug.hs
+++ src/Debug.hs
@@ -3,18 +3,14 @@
 
 module Debug where
 
-import           Data.ByteString         (ByteString)
 import qualified Data.ByteString.Char8   as B
-import           Data.List.Split         (splitOn)
 import           Data.Monoid             ((<>))
-
-
 
 
 -- Debugging. Instrument fns w/ log output to see what's happening.
 -- IMPORTANT!!! Must be absolute path.
 debugFile ∷ FilePath
-debugFile = "/Users/markwong-vanharen/Development/TagFS/debug.log"
+debugFile = "/tmp/tagFS.log"
 
 dbg ∷ String → IO ()
 dbg msg = B.appendFile debugFile (B.pack msg <> "\n")
